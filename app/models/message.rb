b04725cb80f39d982e8cc08dc74566425c652c43
Marcelo Tenorio de Souza <marcelllo.martin@gmail.com>
2020-01-29 21:09:16 -0300

    Implement Action Cable features

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :messagable, polymorphic: true
   belongs_to :user
+
+  after_create_commit { MessageBroadcastJob.perform_later self }
+
   validates_presence_of :body, :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root to: 'teams#index'
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   resources :teams, only: [:create, :destroy]
-  get '/:slug', to: 'teams#show'
   resources :channels, only: [:show, :create, :destroy]
   resources :talks, only: [:show]
   resources :team_users, only: [:create, :destroy]
-  devise_for :users, :controllers => { registrations: 'registrations' }
+
+  get '/:slug', to: 'teams#show'
+
+  mount ActionCable.server => '/cable'
 end
